4c29da7fc734e7e27d7101ad9b88454354152822
Reimund Trost <reimund@code7.se>
2011-04-05 18:09:21 +0200

    Styled the page.

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-$xml = file_get_contents('http://api.met.no/weatherapi/locationforecast/1.8/?lat=57.42;lon=11.55;msl=70');
+// Systemet kappelplatsen http://api.met.no/weatherapi/locationforecast/1.8/?lat=57.69;lon=11.97;msl=10
+$url = 'http://api.met.no/weatherapi/locationforecast/1.8/?lat=57.42;lon=11.55;msl=10';
+$xml = file_get_contents($url);
 
 //$ch = curl_init();
-//curl_setopt($ch, CURLOPT_URL, 'http://api.met.no/weatherapi/locationforecast/1.8/?lat=57.42;lon=11.55;msl=70');
+//curl_setopt($ch, CURLOPT_URL, $url);
 //curl_setopt($ch, CURLOPT_HEADER, false);
 //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //$xml = curl_exec($ch);
@@ -67,17 +69,73 @@ $is_raining = in_array($weather, array(
 
 <head>
 	<meta charset="UTF-8" /> 
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js" type="text/javascript" charset="utf-8"></script> 
 	<title><?php echo $is_raining ? 'Ja, det regnar i Göteborg.' : 'Nej, det regnar inte i Göteborg.'; ?></title>
 
 <style>
+html{height: 100%;}
 body {
+	margin: 0;
+	font-family: 'CabinBold', 'Helvetica', 'Lucida Grande', sans-serif;
+	background: #ebf1ef;
+	height: 100%;
 	text-align: center;
+	background-image: -webkit-gradient(
+		linear,
+		left bottom,
+		left top,
+		color-stop(0, rgb(206,221,216)),
+		color-stop(1, rgb(235,241,239))
+	);
+	background-image: -moz-linear-gradient(
+		center bottom,
+		rgb(206,221,216) 0%,
+		rgb(235,241,239) 100%
+	);
 }
 strong {
-	font-size: 30em;
+	line-height: 0px;
+	opacity: 0;
+	vertical-align: text-top;
+	font-size: 20em;
+	text-shadow: 0 -2px 0px #cad9e9;
+	color: #8fb3a9;
+}
+
+@font-face {
+    font-family: 'CabinBold';
+    src: url('fonts/Cabin-Bold-webfont.eot');
+    src: url('fonts/Cabin-Bold-webfont.eot?#iefix') format('eot'),
+         url('fonts/Cabin-Bold-webfont.woff') format('woff'),
+         url('fonts/Cabin-Bold-webfont.ttf') format('truetype'),
+         url('fonts/Cabin-Bold-webfont.svg#webfontEiid5d2G') format('svg');
+    font-weight: normal;
+    font-style: normal;
 }
 </style>
+
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-22536382-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+
 </head>
+
+<script>
+jQuery(document).ready(function($) {
+	$('strong').css('height', $(window).height());
+	$('strong').delay(100).animate({'line-height': $(window).height() + 'px', opacity: 1}, 1000, function(){});
+});
+</script>
 
 <body>
 	<strong><?php echo $is_raining ? 'Ja' : 'Nej'; ?></strong>
